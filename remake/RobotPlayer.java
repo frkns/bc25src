720dd0ebd54dc229e8cdab469501f96e8303d5b5
Ryan Leong <39171486+Programmer484@users.noreply.github.com>
2025-01-08 14:32:50 -0700

    Added template code

---
+++ remake/RobotPlayer.java
@@ -0,0 +1,32 @@
+package remake;
+
+import battlecode.common.*;
+
+public class RobotPlayer {
+
+    static RobotController rc;
+    @SuppressWarnings("unused")
+    public static void run(RobotController r) throws GameActionException {
+        rc = r;
+
+        while (true) {
+            try {
+                // Move switch statements into phases        
+                switch (rc.getType()){
+                    case SOLDIER: UnitFuncs.runSoldier(rc); break; 
+                    case MOPPER: UnitFuncs.runMopper(rc); break;
+                    case SPLASHER: UnitFuncs.runSplasher(rc); break;
+                    default: TowerFuncs.runTower(rc); break;
+                }
+            } catch (GameActionException e) {
+                System.out.println("GameActionException");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Exception");
+                e.printStackTrace();
+            } finally {
+                Clock.yield();
+            }
+        }
+    }
+}
