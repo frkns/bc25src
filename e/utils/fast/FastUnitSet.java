e24ecd8151dd2b1c3ec9bb3cb6d7290b365d9af2
louis-max-h <louis-max.harter@protonmail.com>
2025-01-17 10:52:29 +0100

    fix fastUnitSet

--- e/utils/fast/FastUnitSet.java
+++ e/utils/fast/FastUnitSet.java
@@ -1,15 +1,15 @@
 package e.utils.fast;
-
 import battlecode.common.RobotInfo;
 
 public class FastUnitSet {
-    char size = 0;
+    char size;
     char maxSize;
     char[] hashmap; // hashmap[id] = index of robot in data.
     RobotInfo[] data;
 
-    public FastUnitSet(char maxLen){
-        maxSize = maxLen;
+    public FastUnitSet(int maxLen){
+        size = 0;
+        maxSize = (char) maxLen;
 
         data = new RobotInfo[maxSize + 1]; // Last element is null
         hashmap = String.valueOf(maxSize).repeat(20000).toCharArray();
@@ -19,7 +19,7 @@ public class FastUnitSet {
 
     public void add(RobotInfo robot){
         // If robot not in data, give index to hashmap[id]
-        if(hashmap[robot.ID] != maxSize) {
+        if(hashmap[robot.ID] == maxSize) {
             if(size == maxSize){
                 size = 0;
                 hashmap = String.valueOf(maxSize).repeat(20000).toCharArray();
@@ -42,14 +42,15 @@ public class FastUnitSet {
     public void pop(int id){
         if(hashmap[id] != maxSize){
 
-            // Replace with last element
-            data[hashmap[id]] = data[size+1];
-            hashmap[ // Update the index of moved element
-                    data[hashmap[id]].ID
-                    ] = hashmap[id]; // That it is now at hashmap[id] location
-
-            hashmap[id] = maxSize; // Pointing to null
             size--;
+            if(size != 0) {
+                // Replace with last element
+                data[hashmap[id]] = data[size];
+                hashmap[ // Update the index of moved element
+                        data[hashmap[id]].ID
+                        ] = hashmap[id]; // That it is now at hashmap[id] location
+            }
+            hashmap[id] = maxSize; // Pointing to null
         }
     }
 }
